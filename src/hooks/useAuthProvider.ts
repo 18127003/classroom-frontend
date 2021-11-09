c4c720c544c322c7c4c55283160ff3ab0afc59d4
18127016 <18127016@student.hcmus.edu.vn>
2021-11-09 12:14:34 +0700

    user info to survive page reload

--- src/hooks/useAuthProvider.ts
+++ src/hooks/useAuthProvider.ts
@@ -1,6 +1,7 @@
 import { Account, AuthRequest } from "@/model/model"
 import { loginUser } from "@/services/service"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
 
 export interface IUseAuthProvider{
     login:(auth:AuthRequest, cb: ()=>void)=>void
@@ -10,13 +11,25 @@ export interface IUseAuthProvider{
 
 const useAuthProvider=()=>{
     const [user, setUser]=useState<null|Account>(null)
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
+
+    useEffect(()=>{
+        setUser(cookies.user)
+    },[])
+
     const login= async (auth:AuthRequest, cb:()=>void)=>{
-        const res = await loginUser(auth);
-        setUser(res.data);
+        const res = (await loginUser(auth)).data;
+        setUser(res);
+        setCookie('user',res,{
+            'path':'/',
+            'maxAge':3600000
+        });
         cb();
     }
     const logout=(cb:()=>void)=>{
         setUser(null);
+        removeCookie('user');
+        cb();
     }
     return {
         user,
