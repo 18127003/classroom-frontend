cae6963e3ad14c6f24a78fbe1ceacc1b98a23a5b
18127016 <18127016@student.hcmus.edu.vn>
2021-11-13 12:50:07 +0700

    copy link to clipboard

--- src/components/Dialog/InviteParticipantDialog.tsx
+++ src/components/Dialog/InviteParticipantDialog.tsx
@@ -2,14 +2,22 @@ import { InviteParticipantDialogProps } from "@/@types/props";
 import useChipEditor from "@/hooks/useChipEditor";
 import { AppState } from "@/reducers";
 import {  ContentCopy } from "@mui/icons-material";
-import { Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, TextField } from "@mui/material";
-import React, { SyntheticEvent } from "react";
+import { Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Grid, IconButton, TextField, Typography } from "@mui/material";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 
 const InviteParticipantDialog: React.FC<InviteParticipantDialogProps>=({title,isStudent, isOpen, handleClose})=>{
     const {values,currentValue, handleDelete, handleChange, handleKeyDown} = useChipEditor();
-    const classroom = useSelector((state: AppState)=>state.detail.detail)
+    const classroom = useSelector((state: AppState)=>state.detail.detail);
+    const [inviteLink, setInviteLink] = useState("");
+    const [copied, setCopied]=useState(false);
+
+    useEffect(()=>{
+        if(classroom){
+            setInviteLink(`https://18127003.github.io/classroom-frontend/classroom/${classroom.id}?invite=true&code=${classroom.code}`)
+        }
+    },[classroom])
 
     const handleSubmit = (event: SyntheticEvent)=>{
         event.preventDefault();
@@ -18,53 +26,63 @@ const InviteParticipantDialog: React.FC<InviteParticipantDialogProps>=({title,is
 
         // dispatch(joinClassroomRequest(target.classroomCode.value));
     }
+
+    const handleCopyClick = ()=>{
+        navigator.clipboard.writeText(inviteLink)
+        setCopied(true)
+    }
+
     return (
         <Dialog open={isOpen} onClose={handleClose} maxWidth='sm' fullWidth={true}>
             <DialogTitle>{title}</DialogTitle>
             <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '96%' },
-            }}
-            autoComplete="on"
-            >
-            <DialogContent>
-            {isStudent&&(<TextField
-                id="link"
-                label="Invite Link"
-                defaultValue={classroom?`https://18127003.github.io/classroom-frontend/classroom/${classroom.id}?invite=true&code=${classroom.code}`:''}
-                InputProps={{
-                    readOnly: true,
-                    endAdornment: (
-                        <IconButton>
-                            <ContentCopy />
-                        </IconButton>
-                    )
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '96%' }
                 }}
-                
-            />)}
-            <Grid container rowSpacing={2} columnSpacing={1} columns={{ xs: 4, sm: 10, md: 10 }} sx={{m:1}}> 
-                {values.map((email, index)=>(
-                    <Grid item  key={index}>
-                        <Chip
-                            label={email}
-                            onDelete={handleDelete(email)}
-                        />
+                autoComplete="on"
+            >
+                <DialogContent>
+                    {isStudent&&(<TextField
+                        id="link"
+                        label="Invite Link"
+                        defaultValue={inviteLink}
+                        InputProps={{
+                            readOnly: true,
+                            endAdornment: (
+                                <IconButton onClick={handleCopyClick}>
+                                    <ContentCopy />
+                                </IconButton>
+                            )
+                        }}
+                    />)}
+                    {copied && 
+                        (<Typography color='green' sx={{m:1}} variant="body2">
+                            Link copied to clipboard
+                        </Typography>)}
+                    <Divider sx={{m:1}}/>
+                    <Grid container rowSpacing={2} columnSpacing={1} columns={{ xs: 4, sm: 10, md: 10 }} sx={{m:1}}> 
+                        {values.map((email, index)=>(
+                            <Grid item  key={index}>
+                                <Chip
+                                    label={email}
+                                    onDelete={handleDelete(email)}
+                                />
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
-            <TextField
-                placeholder="Type or paste email addresses and press `Enter`"
-                value={currentValue}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-            />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button type='submit'>Invite</Button>
-            </DialogActions>
+                    <TextField
+                        placeholder="Type or paste email addresses and press `Enter`"
+                        value={currentValue}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type='submit'>Invite</Button>
+                </DialogActions>
             </Box>
         </Dialog>
     )
